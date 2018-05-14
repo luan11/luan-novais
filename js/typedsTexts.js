b92b1fff1fc546bb8d1eb277be4361b03e2e9170
Luan Novais <luaannovais@live.com>
2018-05-13 22:13:52 -0300

    att styles + typer text

---
+++ js/typedsTexts.js
@@ -0,0 +1,16 @@
+var typedName = new Typed('#name', {
+  strings: ['LUAN NOVAIS DOS SANTOS','LUAN NOVAIS','LUAN'],
+  typeSpeed: 30,
+  backSpeed: 50,
+  backDelay: 700,
+  cursorChar:''
+});
+
+var typedFrase = new Typed('#phrase', {
+  strings: ['" My goal is always to make progress. I can try and fail, but I will never give up. "','" A minha meta é sempre progredir. Posso tentar e fracassar, mas nunca irei desistir. "'],
+  typeSpeed: 25,
+  backSpeed: 10,
+  backDelay: 1200,
+  startDelay: 1300,
+  cursorChar:''
+});
