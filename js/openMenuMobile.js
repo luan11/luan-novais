85ef436caf708082853fe072da79c2878684e760
luan11 <luaannovais@live.com>
2018-05-14 16:04:25 -0300

    slider projects

--- js/openMenuMobile.js
+++ js/openMenuMobile.js
@@ -2,15 +2,12 @@ var btnOpenMenu = $('.btn-openMenu');
 var btnCloseMenu = $('.btn-closeMenu');
 var nav = $('#navPrincipal');
 
-var larguraTela = $(window).width();
-
-window.onload = function(){
-btnOpenMenu.on('click',function(){
+btnOpenMenu.on('click',function(event){
+	event.preventDefault();
 	nav.fadeIn(300).removeClass("mobile-d-none");
 	btnCloseMenu.addClass('mobile-d-block');
 	btnCloseMenu.on('click',function(){
-		nav.fadeOut(200).addClass('mobile-d-none');
+		nav.fadeOut(300).addClass('mobile-d-none');
 		btnCloseMenu.removeClass('mobile-d-block');
 	});
 });
-};
---
+++ js/projectsSlider.js
@@ -0,0 +1,27 @@
+$('.slider-projects').slick({
+  centerMode: true,
+  centerPadding: '50px',
+  arrows: true,
+  dots: true,
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
