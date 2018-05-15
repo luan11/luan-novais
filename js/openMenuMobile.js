db9dcbde5b228d1e8b9ba95a9656e7d0718bdc09
Luan Novais <luaannovais@live.com>
2018-05-14 22:53:10 -0300

    novo efeito do menu

--- js/openMenuMobile.js
+++ js/openMenuMobile.js
@@ -4,10 +4,10 @@ var nav = $('#navPrincipal');
 
 btnOpenMenu.on('click',function(event){
 	event.preventDefault();
-	nav.fadeIn(300).removeClass("mobile-d-none");
+	nav.slideDown(200).removeClass("mobile-d-none");
 	btnCloseMenu.addClass('mobile-d-block');
 	btnCloseMenu.on('click',function(){
-		nav.fadeOut(300).addClass('mobile-d-none');
+		nav.slideUp(200).addClass('mobile-d-none');
 		btnCloseMenu.removeClass('mobile-d-block');
 	});
 });
